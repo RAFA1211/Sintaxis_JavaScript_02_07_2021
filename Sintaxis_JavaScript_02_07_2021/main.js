17377733b603b63365d7b85e7c6b852ab1461598
RAFA1211 <86581811+RAFA1211@users.noreply.github.com>
2021-07-07 18:59:46 -0500

    Add files via upload

---
+++ Sintaxis_JavaScript_02_07_2021/main.js
@@ -0,0 +1,15 @@
+
+nombreString = `Rafael Angel Angarita Rodríguez`;
+edadNumber = 47;
+soyProfesorBolena = false;
+estudiantesAprobadosDiplomadoNull = null;
+cocerUndefined = undefined;
+hobbiesArray = ["Programar","Cocino", 3043640755, null];
+hobbiesArray["Soy profesor"] = soyProfesorBolena;
+dirrecionPersonalObjecte = {
+    dirrecion: "Calle 11",
+    pais: "Colombia",
+    Pais: "Colombia2",
+    mundo: "Tierra",
+    0: "Hola soy el identificador numerico"
+};
